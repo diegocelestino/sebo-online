0aa4427214c2b9a2b83caa911aa8a7204de12fb6
Diego <diegocdsantos@gmail.com>
2023-09-17 18:40:10 -0300

    initial commit

---
+++ src/main/java/com/seboonline/SeboApplication.java
@@ -0,0 +1,13 @@
+package com.seboonline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeboApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SeboApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/seboonline/SeboApplicationTests.java
@@ -0,0 +1,13 @@
+package com.seboonline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeboApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
